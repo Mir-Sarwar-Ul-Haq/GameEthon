1e3af3906f27607bab9ca6a6b8aa2b3073017ba4
SarwarMir <sarwar.marvelminds@gmail.com>
2025-03-11 04:01:55 +0530

    implemented react-query

--- src/components/GameHeading.jsx
+++ src/components/GameHeading.jsx
@@ -1,11 +1,19 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import useGenres from "../hooks/useGenres";
+import usePlatform from "../hooks/usePlatform";
+import useGenre from "../hooks/useGenre";
 
 function GameHeading({ gameQuery }) {
-  const heading = `${gameQuery.platform?.name || ""} ${
-    gameQuery.genre?.name || ""
-  } Games`;
-  return <Typography variant="h4" px={3} pt={2}  >{heading}</Typography>;
+  const genre = useGenre(gameQuery.genreId);
+  const platform = usePlatform(gameQuery.platformId);
+
+  const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
+  return (
+    <Typography variant="h4" px={3} pt={2}>
+      {heading}
+    </Typography>
+  );
 }
 
 export default GameHeading;
--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -1,13 +1,34 @@
-import useData from "./useData";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getAll } from "../services/api-client";
+import ms from "ms";
 
-const useGames = (gameQuery) => useData("/games", {
-    params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-    }
-},
-    [gameQuery]);
+const useGames = (gameQuery) =>
+    useInfiniteQuery({
+        queryKey: ['games', gameQuery],
+        queryFn: ({ pageParam = 1 }) =>
+            getAll('/games', {
+                params: {
+                    genres: gameQuery.genreId,
+                    parent_platforms: gameQuery.platformId,
+                    ordering: gameQuery.sortOrder,
+                    search: gameQuery.searchText,
+                    page: pageParam
+                }
+            }),
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.next ? allPages.length + 1 : undefined;
+        },
+        staleTime: ms("24h"), //24H
+
+    })
+// useData("/games", {
+//     params: {
+//         genres: gameQuery.genre?.id,
+//         platforms: gameQuery.platform?.id,
+//         ordering: gameQuery.sortOrder,
+//         search: gameQuery.searchText
+//     }
+// },
+//     [gameQuery]);
 
 export default useGames;
